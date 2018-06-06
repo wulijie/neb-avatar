523d4f09f75e3160b9fa232d2662d6d8b3fe385e
JackWu <androidzk@163.com>
2018-06-06 18:30:54 +0800

    first init

---
+++ lib-avatar/src/main/java/com/zoke/neb/avatar/Conf.java
@@ -0,0 +1,9 @@
+package com.zoke.neb.avatar;
+
+/**
+ * Created by wulijie on 2018/6/6.
+ */
+public interface Conf {
+    boolean DEBUG = true;
+    //合约地址
+}
---
+++ lib-avatar/src/main/java/com/zoke/neb/avatar/api/NebAvatar.java
@@ -0,0 +1,8 @@
+package com.zoke.neb.avatar.api;
+
+/**
+ * Created by wulijie on 2018/6/6.
+ */
+public class NebAvatar {
+    
+}
